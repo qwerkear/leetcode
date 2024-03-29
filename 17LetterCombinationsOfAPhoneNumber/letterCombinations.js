88cd99e56ab57bad18d396d7a5957ca6ce1cfd60
Jamie <qwer.kear@gmail.com>
2024-03-29 13:22:03 -0400

    Letter combinations js solution

---
+++ 17LetterCombinationsOfAPhoneNumber/letterCombinations.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    let numberMap = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+    let result = []
+
+    let digitsArray = digits.split('')
+
+    let recursiveBackTrack = (i, currentString) => {
+        if (currentString.length == digits.length) {
+            result.push(currentString)
+            return
+        }
+        for (c of numberMap[digits[i]]) {
+            recursiveBackTrack(i + 1, currentString + c)
+        }
+
+    }
+    if (digits) {
+        recursiveBackTrack(0, "")
+    }
+    return result
+};
+
+console.log(letterCombinations("23"))
