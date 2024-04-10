7b03aa4ea57ffa631fb75461f1bee66d2fa7950f
Jamie <qwer.kear@gmail.com>
2024-04-10 14:29:44 -0400

    optimal

--- 80RemoveDuplicatesSortedArrayII/removeDuplicates.js
+++ 80RemoveDuplicatesSortedArrayII/removeDuplicates.js
@@ -24,4 +24,21 @@ var removeDuplicates = function(nums) {
     return validIndex
 };
 
-removeDuplicates([1,1,1,2,2,3])
+
+// The lastValue variable is not necessary because you can always compare nums[i] with nums[validIndex - 1].
+// Also, the valueCount variable can be replaced with a check of validIndex > 1 and nums[validIndex - 2] != nums[i].
+// This way, you ensure that each unique element appears at most twice.
+
+// Here is an optimized solution:
+
+var removeDuplicatesClean = function(nums) {
+    let validIndex = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (validIndex < 2 || nums[i] != nums[validIndex - 2]) {
+            nums[validIndex++] = nums[i];
+        }
+    }
+    return validIndex;
+};
+
+removeDuplicatesClean([1,1,1,2,2,3])
