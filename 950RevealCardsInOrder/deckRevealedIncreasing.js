90c4127115f7b1a92baeab3ed10917e77f89d3fe
Jamie <qwer.kear@gmail.com>
2024-04-10 10:57:15 -0400

    clarify comment

--- 950RevealCardsInOrder/deckRevealedIncreasing.js
+++ 950RevealCardsInOrder/deckRevealedIncreasing.js
@@ -19,4 +19,4 @@ deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7])
 
 //Easier to keep track of indexes that have not been filled.
 //We fill the result array by index using the sorted deck.
-//We fill the result index with the next highest value and then, move the NEXT result index to the back
+//We fill the result at the current index with the next highest value and then move the following index to the back
