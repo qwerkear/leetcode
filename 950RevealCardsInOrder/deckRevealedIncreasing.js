d1a1d3c372b5be5277bd2fc4077325be631bba89
Jamie <qwer.kear@gmail.com>
2024-04-10 10:54:10 -0400

    format

--- 950RevealCardsInOrder/deckRevealedIncreasing.js
+++ 950RevealCardsInOrder/deckRevealedIncreasing.js
@@ -2,11 +2,11 @@
  * @param {number[]} deck
  * @return {number[]}
  */
-var deckRevealedIncreasing = function(deck) {
-    deck.sort((a,b) => a - b)
+var deckRevealedIncreasing = function (deck) {
+    deck.sort((a, b) => a - b)
     let resultIndexes = [...Array(deck.length).keys()]
     let result = []
-    while(resultIndexes[0] != undefined){
+    while (resultIndexes[0] != undefined) {
         result[resultIndexes[0]] = deck.shift()
         resultIndexes.shift()
         resultIndexes.push(resultIndexes.shift())
@@ -15,7 +15,7 @@ var deckRevealedIncreasing = function(deck) {
     return result
 };
 
-deckRevealedIncreasing([17,13,11,2,3,5,7])
+deckRevealedIncreasing([17, 13, 11, 2, 3, 5, 7])
 
 //Easier to keep track of indexes that have not been filled.
 //We fill the result array by index using the sorted deck.
