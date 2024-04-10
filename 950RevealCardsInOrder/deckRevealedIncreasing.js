ec9d49e820a1ed96de26d05cc7e18945f067bbb7
Jamie <qwer.kear@gmail.com>
2024-04-10 10:53:58 -0400

    deck reveal increasing js solution

---
+++ 950RevealCardsInOrder/deckRevealedIncreasing.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} deck
+ * @return {number[]}
+ */
+var deckRevealedIncreasing = function(deck) {
+    deck.sort((a,b) => a - b)
+    let resultIndexes = [...Array(deck.length).keys()]
+    let result = []
+    while(resultIndexes[0] != undefined){
+        result[resultIndexes[0]] = deck.shift()
+        resultIndexes.shift()
+        resultIndexes.push(resultIndexes.shift())
+    }
+
+    return result
+};
+
+deckRevealedIncreasing([17,13,11,2,3,5,7])
+
+//Easier to keep track of indexes that have not been filled.
+//We fill the result array by index using the sorted deck.
+//We fill the result index with the next highest value and then, move the NEXT result index to the back
