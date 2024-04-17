96c800134c56180e52b9c84cd716496418091a41
Jamie <qwer.kear@gmail.com>
2024-04-17 13:41:27 -0400

    merge practice

--- 88MergeSortedArray/merge.js
+++ 88MergeSortedArray/merge.js
@@ -5,41 +5,42 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
     let j = 0
-    for(let i = m; i < nums1.length; i++){
+    for (let i = m; i < nums1.length; i++) {
         nums1[i] = nums2[j]
         j++
     }
-    nums1.sort((a,b) => a - b)
+    nums1.sort((a, b) => a - b)
 
 };
 
-merge([1,2,3,0,0,0], 3, [2,5,6], 3)
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
 
 //I'm assuming below is how you're supposed to do it without just calling .sort() such as above
 //This seems less readable, but is probably the more correct solution since you can explain everything
 //I couldn't go into too much detail about how exactly .sort() works under the hood in an interview, but below is easy to explain
 
-var mergeButWithLessMemory = function(nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
+var mergeBetter = function (nums1, m, nums2, n) {
+    let modifyingIndex = m + n - 1;
+    m = m - 1
+    n = n - 1
 
-    while (i >= 0 && j >= 0) {
-        if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i];
-            i--;
+    while (m >= 0 && n >= 0) {
+        if (nums1[m] > nums2[n]) {
+            nums1[modifyingIndex] = nums1[m]
+            modifyingIndex--
+            m--
         } else {
-            nums1[k] = nums2[j];
-            j--;
+            nums1[modifyingIndex] = nums2[n]
+            modifyingIndex--
+            n--
         }
-        k--;
     }
 
-    while (j >= 0) {
-        nums1[k] = nums2[j];
-        j--;
-        k--;
+    while (n >= 0) {
+        nums1[modifyingIndex] = nums2[n]
+        n--
+        modifyingIndex--
     }
-};
+}
