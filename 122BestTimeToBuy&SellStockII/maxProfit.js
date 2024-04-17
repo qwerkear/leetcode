3cd879e143bc14096c11d24fba76e25d0d348d7f
Jamie <qwer.kear@gmail.com>
2024-04-16 23:14:08 -0400

    Buy & sell I & II

---
+++ 122BestTimeToBuy&SellStockII/maxProfit.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let localMin = Infinity
+    //let localMinIndex = 0
+    let total = 0
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < localMin) {
+            localMin = prices[i]
+        }
+        if (i !== 0 && prices[i - 1] < prices[i]) {
+            localMin = prices[i]
+            total += prices[i] - prices[i - 1]
+            localMin = Infinity
+        }
+    }
+
+    return total
+
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]))
