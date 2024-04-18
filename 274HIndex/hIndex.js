25db5796f9aef9f7c35f7f38edd8144ba348f1c5
Jamie <qwer.kear@gmail.com>
2024-04-18 14:14:17 -0400

    hIndex optimal js solution

--- 274HIndex/hIndex.js
+++ 274HIndex/hIndex.js
@@ -2,22 +2,35 @@
  * @param {number[]} citations
  * @return {number}
  */
-var hIndex = function(citations) {
+var hIndex = function (citations) {
     let h = citations.length
-    while( h >= 0){
+    while (h >= 0) {
         let hCount = 0
-        for(let i = 0; i < citations.length; i++){
-            if(citations[i] >= h){
+        for (let i = 0; i < citations.length; i++) {
+            if (citations[i] >= h) {
                 hCount++
             }
         }
-        if(hCount >= h){
+        if (hCount >= h) {
             return h
         }
         h--
     }
 };
 
-hIndex([3,0,6,1,5])
+hIndex([3, 0, 6, 1, 5])
 
-//This appears slow in leetcode, maybe I should sort.
+//This appears slow/inefficient in leetcode, maybe I should sort.
+
+//This looks better
+var hIndex = function (citations) {
+    citations = citations.sort((a, b) => b - a)
+    let h = 0
+
+    while (h < citations.length && citations[h] > h) {
+        h++
+    }
+
+    return h
+
+};
